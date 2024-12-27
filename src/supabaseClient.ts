df6cd61e3b2c41c9ea5cd80fcdff5c7de72e2c11
himalf <himal.fullel15@gmail.com>
2024-12-27 20:06:09 +0545

    fix: solve the api url and api key not found error

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import { config } from 'dotenv';
+config();
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY,
