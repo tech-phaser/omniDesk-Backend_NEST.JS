18a72d0a07a0120a46e88971d93c803a4a403e2e
himalf <himal.fullel15@gmail.com>
2024-12-27 20:03:01 +0545

    feat: use post method for both signin and signup and call their methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
+import { SignInDto } from './dto/signin.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup') // http://localhost:3000/auth/signup
+  signup(@Body() signUpDto: SignupDto) {
+    return this.authService.signup(signUpDto);
+  }
+  @Post('signin')
+  signin(@Body() signInDto: SignInDto) {
+    return this.authService.signin(signInDto);
+  }
+}
