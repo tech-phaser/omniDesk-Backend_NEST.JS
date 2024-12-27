199d00b6ebf9a3c5476159c6df6b2d491535eb01
himalf <himal.fullel15@gmail.com>
2024-12-27 20:35:04 +0545

    chore: add some todo stuffs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-
+//TODO: auto genearte username
+//TODO: Phone number and OTP
+//TODO: date of birth or age
+// TODO: Image and other stuffs
 @Module({
   imports: [AuthModule],
 })
