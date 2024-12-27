9f974bc52dbc86d9c9cf65fc57bd8ea26f91e3db
himalf <himal.fullel15@gmail.com>
2024-12-27 22:29:39 +0545

    fix: cors error and port error

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { config } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.use(cors());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
+  console.log(`Server is running on port: ${process.env.PORT ?? 4000}`);
 }
 bootstrap();
